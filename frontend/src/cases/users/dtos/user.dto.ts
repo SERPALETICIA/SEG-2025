9105015e297e3ea45118dd8f9ec6bdedfb3f8abe
SERPALETICIA <127907504+SERPALETICIA@users.noreply.github.com>
2025-11-07 21:56:11 -0300

    Aula 07-11

---
+++ frontend/src/cases/users/dtos/user.dto.ts
@@ -0,0 +1,12 @@
+export interface UserDTO{
+    id?: string;
+    name: string;
+    email: string;
+    supabaseId: string;
+    hasMFA: boolean;
+
+    factorId?: string;
+    isMFAValidated?: boolean;
+    isFirstmfaaCCESS?: boolean;
+
+}
